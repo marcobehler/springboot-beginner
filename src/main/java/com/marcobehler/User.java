694bae9cbccdd585e3212534f6f868cbf0de233e
BEHLEMA <marco.behler@kvb.de>
2016-12-12 13:30:33 +0100

    03-registration

---
+++ src/main/java/com/marcobehler/User.java
@@ -0,0 +1,10 @@
+package com.marcobehler;
+
+import java.util.Date;
+
+public class User {
+
+    public String name, email;
+
+    public Date registrationDate;
+}
--- src/main/java/com/marcobehler/UserController.java
+++ src/main/java/com/marcobehler/UserController.java
@@ -1,15 +1,22 @@
 package com.marcobehler;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 public class UserController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String helloWorld() {
-        return "Yay, it works!";
+    public String helloWorld(@RequestParam(defaultValue = "Anonymous")  String name) {
+        return "Hi " + name + ", it works!";
     }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public User registration(@RequestBody  User user) {
+        user.registrationDate = new Date();
+        return user;
+    }
+
 }
